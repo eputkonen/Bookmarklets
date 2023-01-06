d52387a35db68526c06cfd128ce0d46b8be7d93f
Eric Putkonen <eputkonen@users.noreply.github.com>
2023-01-06 13:01:31 -0500

    Create nameORidONtwitter.js
    Cross-reference name or id on Twitter

---
+++ nameORidONtwitter.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bjavascript%3A(function(w%2Cd) %7Bw.open('https%3A%2F%2Ftwitter.com%2Fsearch%3Fsrc%3Dtyped%5Fquery%26f%3Duser%26q%3D'%2BencodeURIComponent(d.getSelection())%2C '_blank')%3B%7D)(window%2Cdocument)%7D)()
