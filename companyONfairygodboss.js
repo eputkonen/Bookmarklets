d97ee688f08cbce63cbaa40c993fe3663b8b02ea
Eric Putkonen <eputkonen@users.noreply.github.com>
2023-01-06 13:04:18 -0500

    Create companyONfairygodboss.js
    Cross-reference company name on Fairy God Boss.

---
+++ companyONfairygodboss.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bjavascript%3A(function(w%2Cd) %7Bw.open('https%3A%2F%2Ffairygodboss.com%2Fsearch%3Ftype%3Dcompany%26term%3D'%2BencodeURIComponent(d.getSelection())%2C '_blank')%3B%7D)(window%2Cdocument)%7D)()
