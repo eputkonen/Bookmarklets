5f35ac021b21e86e5e739588667e6de641ae0c42
Eric Putkonen <eputkonen@users.noreply.github.com>
2023-01-06 13:02:09 -0500

    Create nameONfacebook.js
    Cross-reference name on Facebook

---
+++ nameONfacebook.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bjavascript%3A(function(w%2Cd) %7Bw.open('https%3A%2F%2Fwww.facebook.com%2Fsearch%2Fpeople%2F%3Fq%3D'%2BencodeURIComponent(d.getSelection())%2C '_blank')%3B%7D)(window%2Cdocument)%7D)()
