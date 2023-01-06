2dfd12c28ea5baff73056fdad1d5c242dc5b4ab0
Eric Putkonen <eputkonen@users.noreply.github.com>
2023-01-06 12:54:38 -0500

    Create TechStackWappalyzer.js
    Tech Stack analyzer by domain name through Wappalyzer

---
+++ TechStackWappalyzer.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bjavascript%3A(function()%7Bvar p%3Dprompt('Enter domain name%3A'%2C'')%3Bif(p)%7Bdocument.location.href%3D'https%3A%2F%2Fwww.wappalyzer.com%2Flookup%2F'%2Bescape(p)%7D%7D)()%7D)();
