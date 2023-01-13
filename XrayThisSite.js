ade1d576f30e91188228ccbfbd46701309aa52bd
Eric Putkonen <eputkonen@users.noreply.github.com>
2023-01-13 09:07:34 -0500

    Delete XrayThisSite.js

--- XrayThisSite.js
+++
@@ -1,1 +0,0 @@
-javascript:q=""+(window.getSelection?window.getSelection():document.getSelection?document.getSelection():document.selection.createRange().text);if(!q)q=prompt("Search terms %5Bleave selection and box blank to list all pages%5D ...").replace(/%5Cs%5C+/g,"%252B");if(q!=null)location="http://www.google.com/search?q="+q.replace(/%5Cs+/g,"+")+"+site:"+location.hostname;void(0);
